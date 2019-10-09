a4172fe3e7eccd6a69907dccd7dd70e766b34ae1
Pulho <Pulho@github.com>
2019-10-09 00:52:13 -0300

    Added '&&' possibility and flag for ls command

--- class_simulator.h
+++ class_simulator.h
@@ -4,8 +4,15 @@ class Simulator
 		std::string pathName;
 		std::vector<std::string> windowsInst;
         std::vector<std::string> linuxInst;
+
+        bool notFound = false;
+        bool directoryCommand = false;
+        bool newlineRequire = false;
+        bool andFlag = false;
+
 	public:
 		Simulator();
+        void appendVector(std::vector<std::string>);
         std::string dir();
         void run();
         void decoder(std::string);
@@ -33,6 +40,27 @@ std::string setPathName()
 	remove("path.txt");
 
     return path;
+}
+
+std::string substitutionBar(std::vector<std::string> dir, std::string path)
+{
+    path.pop_back();
+    size_t size = dir.size();
+    std::string newDir = path;
+
+    for(int i = 1; i < size; i++)
+    {
+        if(dir[1][i] == '/')
+        {
+            newDir.push_back('\\');
+        }
+        else
+        {
+            newDir.push_back(dir[1][i]);
+        }
+    }
+
+    return newDir;
 }
 
 Simulator::Simulator()
@@ -71,7 +99,7 @@ void Simulator::decoder(std::string instr)
     this->linuxInst.push_back(substr);
 }
 
-void systemCall(std::vector<std::string> systemVector)
+int systemCall(std::vector<std::string> systemVector)
 {
     size_t size = systemVector.size();
     std::string systemFunc;
@@ -81,46 +109,221 @@ void systemCall(std::vector<std::string> systemVector)
         systemFunc += systemVector[i];
         systemFunc += " ";
     }
-    system(systemFunc.c_str());
+    return system(systemFunc.c_str());
+}
+
+std::string commandConvert(std::string command)
+{
+    if(command == "ls")
+    {
+        return "dir /b ";
+    }
+    else if(command == "clear")
+    {
+        return "cls ";
+    }
+    else if(command == "cat")
+    {
+        return "type ";
+    }
+    else if(command == "mkdir")
+    {
+        return "mkdir ";
+    }
+    else if(command == "touch")
+    {
+        return "NUL > ";
+    }
+    else if(command == "pwd")
+    {
+        return "cd ";
+    }
+    else if(command == "cd")
+    {
+        return "cd ";
+    }  
+    else
+    {
+        return "NONE";
+    }
+}
+
+std::vector<std::string> lsFlags(std::vector<std::string> linuxInst)
+{
+    size_t size = linuxInst.size();
+    size_t auxSize;
+    std::vector<std::string> newFlag;
+    std::vector<std::string> returnString;
+    std::vector<std::string> auxString;
+
+    if(size > 1) // Flags check
+    {
+        for(int i = 1; i < size; i++)
+        {
+            if(linuxInst[i] == "-r") // Reverse = /O:-
+            {
+                returnString.push_back("/O:-N ");
+            }
+            else if(linuxInst[i] == "-a") // Hidden Files = /A:H
+            {
+                returnString.push_back("/A:H ");
+            }
+            else if(linuxInst[i] == "&&" || linuxInst[i] == "&")
+            {
+                if((i + 1) < size)
+                {
+                    returnString.push_back("&& ");
+
+                    if(linuxInst[i+1] != "NONE")
+                    {
+                        returnString.push_back(commandConvert(linuxInst[i+1]));
+                        
+                        for(int y = (i+1); y < size; y++)
+                        {
+                            newFlag.push_back(linuxInst[y]);
+                        }
+
+                        auxString= lsFlags(newFlag);
+                        auxSize = auxString.size();
+
+                        for(int y = 0; y < auxSize; y++)
+                        {
+                            returnString.push_back(auxString[y]);
+                        }
+                        break;
+                    }
+                    else
+                    {
+                        std::cerr << "comando nao e reconhecido como um comando interno\nou externo, um programa operavel ou um arquivo em lotes." << std::endl;  
+                        returnString.clear();
+                        returnString.push_back("NONE");
+                        return returnString;
+                    }
+                }
+                else
+                {
+                    std::cerr << "Sintaxe do comando esta incorreta" << std::endl; 
+                    returnString.clear();
+                    returnString.push_back("NONE");
+                    return returnString;
+                }
+            }
+        }
+        return returnString;
+    }   
+}
+
+void Simulator::appendVector(std::vector<std::string> commands)
+{
+    size_t size = commands.size();
+
+    for(int i = 0; i < size; i++)
+    {
+        this->windowsInst.push_back(commands[i]);
+    }
 }
 
 void Simulator::run()
 {
+    int index = 0;
     size_t size = this->linuxInst.size();
-    bool notFound = false;
-
+    std::vector<std::string> returned;
+    std::string receive;
+    this->notFound = true;
 
     if(size >= 1)
     {
+        receive = commandConvert(this->linuxInst[0]);
+
         if(this->linuxInst[0] == "ls")
         {
-            this->windowsInst.push_back("dir /b");
+            this->windowsInst.push_back(receive); // Default   
+            if(size > 1)
+            {
+                returned = lsFlags(this->linuxInst);
+                if(returned[0] != "NONE")
+                {
+                    this->appendVector(returned);
+                    this->notFound = false;
+                }
+                else
+                {
+                    this->notFound = true;
+                }
+            }
+            else
+            {
+                this->notFound = false;
+            }
         }
-        else if(this->linuxInst[0] == "clear")
+        
+        if(this->linuxInst[0] == "clear")
         {
-            this->windowsInst.push_back("cls");
+            this->windowsInst.push_back(receive);
+            this->notFound = false;
         }
-        else if(this->linuxInst[0] == "cat")
+        /*
+        if(this->linuxInst[0] == "cat")
         {
-            this->windowsInst.push_back("type");
-            this->windowsInst.push_back(this->linuxInst[1]);
+            this->windowsInst.push_back(receive);
+            this->windowsInst.push_back(this->linuxInst[index+1]);
+            this->newlineRequire = true;
+            this->notFound = false;
         }
-        else if(this->linuxInst[0] == "mkdir")
+        */
+        if(this->linuxInst[0] == "mkdir")
         {
-            this->windowsInst.push_back("mkdir");
-            this->windowsInst.push_back(this->linuxInst[1]);
+            this->windowsInst.push_back(receive);
+            this->windowsInst.push_back(this->linuxInst[index+1]);
+            this->notFound = false;
         }
-        else
+        /*
+        if(this->linuxInst[0] == "touch")
         {
-            std::cerr << (char)39 << this->windowsInst[0] << (char)39 << " nao e reconhecido como um comando interno\nou externo, um programa operavel ou um arquivo em lotes." << std::endl; 
-            notFound = true;
+            this->windowsInst.push_back(receive);
+            this->windowsInst.push_back(this->linuxInst[index+1]);
+            this->notFound = false;
         }
+        */
+        if(this->linuxInst[0] == "pwd")
+        {
+            this->windowsInst.push_back(receive);
+            this->newlineRequire = true;
+            this->notFound = false;
+        }
+        /*
+        if(this->linuxInst[0] == "cd")
+        {
+            this->windowsInst.push_back(receive);
+            this->windowsInst.push_back(this->linuxInst[index+1]);
+            
+            this->directoryCommand = true;
 
-        if(!notFound)
+            if(systemCall(this->windowsInst) != 0)
+            {
+                this->notFound = false;
+                this->pathName = substitutionBar(this->linuxInst, this->pathName); 
+            }
+        }
+        */
+        if(!this->notFound && !this->directoryCommand)
         {
             systemCall(this->windowsInst);
+            if(newlineRequire)
+            {
+                std::cout << std::endl;
+            }
+        }
+
+        if(this->notFound == true)
+        {
+            std::cerr << "Comando nao pode ser executado" << std::endl; 
+            this->notFound = true;
         }
     }
+    this->notFound = true;
+    this->directoryCommand = false;
+    this->newlineRequire = false;
     this->windowsInst.clear();
     this->linuxInst.clear();
 }
